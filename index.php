49d78d62542a4953a1d6acf1c29c96d220f2de47
Joshua Ricarder Oducado <31839332+projectvelox@users.noreply.github.com>
2018-08-07 00:19:11 +0800

    initial update

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<?php include 'view/partials/header.php'?>
+
+<body>
+	<!-- Navbar -->
+	<?php include 'view/partials/navbar.php'?>			
+
+	<!-- Background image -->
+	<div class="caipl-background mb-3" id="background-slider-1">
+		<h1 class="text-white">Computer Aided Instruction for Philippine Literature</h1>
+		<p></p>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-6">
+				<h2>Vision</h2>
+				<p>Center of Academic Excellence Delivering Quality Service to all</p>
+			</div>	
+			<div class="col-xs-12 col-md-6">
+				<h2>Mission</h2>
+				<p>Capiz State University is committed to provide advance knowledge and innovation; develop skills, talents and values; undertake relevant research, development and extension services; promote entrepreneurship and environmental consciousness; and enhance industry collaboration and linkages with partner agencies.</p>
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
---
+++ view/partials/header.php
@@ -0,0 +1,11 @@
+<head>
+	<title>Home - Computer Aided Instruction for Philippine Literature</title>
+	<link rel="icon" href="assets/logo/icon.png" type="image/gif" sizes="16x16">
+
+	<!-- Libraries -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+</head>
