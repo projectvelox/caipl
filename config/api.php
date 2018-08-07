c706ca288d228551b9fd7d94925eec31de155d08
Makoy <neimarkbraga@gmail.com>
2018-08-07 23:38:59 +0800

    effin login and logout

--- config/api.php
+++ config/api.php
@@ -1,19 +1,65 @@
-<?php 
+<?php
+    session_start();
 	include 'config-file.php';
 
-	if($_POST["action"]=="create-account"){
-		$studentid = $_POST['studentid'];
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$firstname = $_POST['firstname'];
-		$middlename = $_POST['middlename'];
-		$lastname = $_POST['lastname'];
 
-		$sql = "INSERT INTO accounts(idnumber, username, password, firstname, middlename, lastname, typeofaccount) VALUES ('$studentid', '$username', '$password', '$firstname', '$middlename', '$lastname', '1')";
-		$result = mysqli_query($con,$sql);
+    header('Content-Type: application/json');
+	switch ($_POST["action"]) {
 
-        header('Content-Type: application/json');
-		if($result) echo json_encode(['message' => 'Successfully created your account <b>'.$_POST['username'].'</b>']);
-        else echo json_encode(['error' => ['DB_ERROR', mysqli_error($con)]]);
-	}
-?>
+
+	    // Create Account
+	    case 'create-account':
+            $studentid = $_POST['studentid'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $firstname = $_POST['firstname'];
+            $middlename = $_POST['middlename'];
+            $lastname = $_POST['lastname'];
+
+            $sql = "INSERT INTO accounts(idnumber, username, password, firstname, middlename, lastname, typeofaccount) VALUES ('$studentid', '$username', '$password', '$firstname', '$middlename', '$lastname', '1')";
+            $result = mysqli_query($con,$sql);
+
+            if($result) echo json_encode(['message' => 'Successfully created your account <b>'.$_POST['username'].'</b>']);
+            else echo json_encode(['error' => ['DB_ERROR', mysqli_error($con)]]);
+            break;
+
+
+        // Login Account
+        case 'login-account':
+            $username = mysqli_real_escape_string($con, $_POST['username'] ?: '');
+            $password = $_POST['password'] ?: '';
+            $sql = "
+                SELECT * 
+                FROM accounts
+                WHERE username='$username'
+            ";
+            $result = mysqli_query($con,$sql);
+
+            if($result) {
+                $account = mysqli_fetch_assoc($result);
+                if($account) {
+                    if($account['password'] == $password) {
+                        $_SESSION['account'] = $account;
+                        $redirect = '';
+                        switch ($account['typeofaccount']) {
+                            case '1': $redirect = 'user-dashboard.php'; break;
+                            case '2': $redirect = 'admin-dashboard.php'; break;
+                        }
+                        echo json_encode(['redirect' => $redirect]);
+                    }
+                    else {
+                        echo json_encode(['error' => ['WRONG_PASSWORD', 'Wrong Password.']]);
+                    }
+                }
+                else {
+                    echo json_encode(['error' => ['WRONG_USERNAME', 'Username does not exist.']]);
+                }
+            }
+            else echo json_encode(['error' => ['DB_ERROR', mysqli_error($con)]]);
+            break;
+
+        // Action Not Found
+        default:
+            echo json_encode(['error' => ['ACTION_NOT_FOUND', 'Action not found.']]);
+            break;
+    }
