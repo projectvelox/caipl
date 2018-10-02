f106b0d8dab3918be8db8779c0d560127be6831d
Joshua Ricarder Oducado <31839332+projectvelox@users.noreply.github.com>
2018-10-02 10:29:09 +0800

    asd initial pushed
    a

---
+++ chapter3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<?php include 'view/partials/header.php'?>
+
+<body>
+  <?php 
+    // include 'view/partials/modal-notification.php';
+    // include 'view/partials/modal-registration.php'; 
+    include 'view/partials/navbar.php';
+  ?>      
+
+  <nav aria-label="breadcrumb">
+	  <ol class="breadcrumb bg-dark">
+	    <li class="breadcrumb-item caipl-link"><a href="user-dashboard.php">User Dashboard</a></li>
+	    <li class="breadcrumb-item active text-warning" aria-current="page">Take Lessons</li>
+	  </ol>
+	</nav>
+  
+  <div class="container mt-8 mh75vh" id="bad-background">
+  	<div class="row">
+		<div class="col-md-8">
+			<h2>KABANATA  III</h2>
+			<p><strong>Panahon ng mga Kastila</strong></p>
+			<p>KALIGIRANG KASAYSAYAN</p>
+			<p>
+				Ang isinasaalang-alang na unang pananakop ng mga Kastila sa ating kapuluan ay ang pananatili rito ni Miguel Lopez de Legazpi  1565, bilang kauna unahang kastialng gobernador  heneral.  At ditto nagsimula  ang panitikan ng mga tao rito. Ang diwa ng pasimulang  ito ay nagpatuloy ng walang pagbabago  hanggang sa pagkakaroon ng digmaan at sa kabite noong 1872. Higit sa tatlong dantaon din ang pananakop  na ito ng mga kastila. <br><br>
+				Sa mga panahong iyon, maraming  pagbabago ang naganap sa buhay ng mga Pilipino. Tinangkilik nila ang relihiyong katoliko. Nagpalit sila ng mga pangalan at nagpabinyag. Nagbago ang anyo ng kanilang pamamahay .  Nagkaroon ng mga  bahay na bato at tisa, mga magandang kasangkapan tulad ng piyano, muwebles, at mga kagamitang pangkusina. Nagkaroon  din ng mga sasakyang tulad ng karwahe, tren, at bapor. Natuto  silang magdiwang ng mga kapistahan bilang parangal sa mga santo, sa papa, at sa gobernador. Bilang libangan, nagkaroon ng mga sabong, karera ng kabayo  at teatro.<br><br>
+				Ang mga pagbabagong ito ay nagbigay din ng daan sa pagkakabuo ng ilang pangkat ng ‘’may kaya ‘’ na may mga ari arian  at lupain. May ilan din sa mga Pilipino ang nakapag aral ng kurso tulad ng medisina, abugasya, agrikultura, at pagiging maestro. Ang mga kursong ito ay halos natapos  nila rito sa pilipinas sapagkat marami na rin namang paaralan ang naitatag ng mga panahong iyon.
+
+			</p>
+		</div>
+		<div class="col-md-4">
+			<h2>Lesson Attachments</h2>
+		  	<a href="documentation/KABANATA-III.docx">KABANATA-III.docx</a><br>
+		  	<a href="documentation/Leron Leron Sinta Animated (Filipino  Tagalog Folk Song)  Awiting Pambata.mp3" target="_blank">Leron Leron Sinta Animated (Filipino  Tagalog Folk Song)  Awiting Pambata.mp3</a>
+		</div>
+	</div>
+  </div>
+  
+</body>
+</html>
