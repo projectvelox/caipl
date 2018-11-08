af1ba30428eafc06dac4cfa685dead5a0024db2e
projectvelox <43872288+projectvelox@users.noreply.github.com>
2018-11-08 16:13:12 +0800

    update on scores

--- config/ajax.php
+++ config/ajax.php
@@ -19,7 +19,8 @@ switch ($_POST["action"]) {
         $insertresult = mysqli_query($con,$insertsql);
 
         while ($row = mysqli_fetch_array($result)) {                            
-            $answer = $row['answer'];            
+            $answer = $row['answer'];
+
             $q = $_POST['optradio_'.$row["id"]];
 
             $question = $row["quiz_question"];
@@ -35,18 +36,28 @@ switch ($_POST["action"]) {
                 switch ($q) {
                     case '1':
                         $answers = $row['option_1'];
-                        $answer = $row['option_1'];
                         break;
                     case '2':
                         $answers = $row['option_2'];
-                        $answer = $row['option_2'];
                         break;
                     case '3':
                         $answers = $row['option_3'];
-                        $answer = $row['option_3'];
                         break;
                     case '4':
                         $answers = $row['option_4'];
+                        break;
+                }
+                switch ($answer) {
+                    case '1':
+                        $answer = $row['option_1'];
+                        break;
+                    case '2':
+                        $answer = $row['option_2'];
+                        break;
+                    case '3':
+                        $answer = $row['option_3'];
+                        break;
+                    case '4':
                         $answer = $row['option_4'];
                         break;
                 }
